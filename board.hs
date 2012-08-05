59281483209afb2e013c1f2aac126317b41d5d04
Drew Gross <drew.a.gross@gmail.com>
2012-08-05 16:23:08 -0400

    refactor

--- board.hs
+++ board.hs
@@ -44,14 +44,14 @@ safeBoardAccess board x y
 	| otherwise = [boardAccess board x y]
 
 corners :: Board -> Int -> Int -> [Color]
-corners (Board board size) x y 
-	| x < 0 || x > size || y < 0 || y > size = error "index out of range"
-	| otherwise = (safeBoardAccess (Board board size) (x-1) (y-1)) ++ (safeBoardAccess (Board board size) (x-1) (y+1)) ++ (safeBoardAccess (Board board size) (x+1) (y-1)) ++ (safeBoardAccess (Board board size) (x+1) (y+1))
+corners board x y 
+	| x < 0 || x > (size board) || y < 0 || y > (size board) = error "index out of range"
+	| otherwise = (safeBoardAccess board (x-1) (y-1)) ++ (safeBoardAccess board (x-1) (y+1)) ++ (safeBoardAccess board (x+1) (y-1)) ++ (safeBoardAccess board (x+1) (y+1))
 
 sides :: Board -> Int -> Int -> [Color]
-sides (Board board size) x y
-	| x < 0 || x >= size || y < 0 || y >= size = error "index out ofrange"
-	| otherwise = (safeBoardAccess (Board board size) (x-1) (y)) ++ (safeBoardAccess (Board board size) (x+1) (y)) ++ (safeBoardAccess (Board board size) (x) (y-1)) ++ (safeBoardAccess (Board board size) (x) (y+1))
+sides board x y
+	| x < 0 || x >= (size board) || y < 0 || y >= (size board) = error "index out ofrange"
+	| otherwise = (safeBoardAccess board (x-1) (y)) ++ (safeBoardAccess board (x+1) (y)) ++ (safeBoardAccess board (x) (y-1)) ++ (safeBoardAccess board (x) (y+1))
 
 isOpenToColor :: Board -> Color -> Int -> Int -> Bool
 isOpenToColor board color 0 0 = (array board) !! 0 == Empty
