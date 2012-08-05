c764d987506494d574b2a24c149b9586c0d58cc3
Drew Gross <drew.a.gross@gmail.com>
2012-08-05 01:05:26 -0400

    kinda solid board representation

--- board.hs
+++ board.hs
@@ -1,17 +1,22 @@
+data Color = Yellow | Red | Green | Blue | Empty deriving (Show, Eq)
+data Board = Board [[Color]] deriving (Show)
+
+newBoard = take 20 $ repeat $ take 20 $ repeat Empty
+
 safeAccess :: [a] -> Int -> [a]
 safeAccess l i = if ((length l) <= i || i < 0)
 					then []
 					else [l !! i]
 
-safeAccess2D :: [[a]] -> Int -> Int -> [a]
-safeAccess2D l x y = safeAccess (if length (safeAccess l x) > 0 then (safeAccess l x) !! 0 else []) y
+safeAccess2D :: Board -> Int -> Int -> [Color]
+safeAccess2D (Board board) x y = safeAccess (if length (safeAccess board x) > 0 then (safeAccess board x) !! 0 else []) y
 
+corners :: Board -> Int -> Int -> [Color]
 corners board x y = (safeAccess2D board (x-1) (y-1)) ++ (safeAccess2D board (x-1) (y+1)) ++ (safeAccess2D board (x+1) (y-1)) ++ (safeAccess2D board (x+1) (y+1))
 
+sides :: Board -> Int -> Int -> [Color]
 sides board x y = (safeAccess2D board (x-1) (y)) ++ (safeAccess2D board (x+1) (y)) ++ (safeAccess2D board (x) (y-1)) ++ (safeAccess2D board (x) (y+1))
 
-
-isOpen board 0 0 = not (board !! 0 !! 0)
-isOpen board x y = (True `elem` (corners board x y)) && (not (True `elem` sides board x y))
-
-board = take 20 (repeat (take 20 $ repeat False))
+isOpenToColor :: Board -> Color -> Int -> Int -> Bool
+isOpenToColor (Board board) color 0 0 = board !! 0 !! 0 == Empty
+isOpenToColor board color x y = (color `elem` (corners board x y)) && (not (color `elem` sides board x y))
