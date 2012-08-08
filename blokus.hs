1dff495f226118e3c7ae58b78508f3986f9ff271
Drew Gross <drew.a.gross@gmail.com>
2012-08-07 22:51:56 -0400

    refactoring

---
+++ blokus.hs
@@ -0,0 +1,85 @@
+import Debug.Trace
+import Board
+
+
+isInPiece :: Piece -> BoardPoint -> Bool
+isInPiece piece (BoardPoint x y) = x >= 0 && x < width piece && y >= 0 && y < height piece
+
+plus :: BoardPoint -> BoardPoint -> BoardPoint
+plus (BoardPoint x1 y1) (BoardPoint x2 y2) = BoardPoint (x1 + x2) (y1 + y2)
+
+data Piece = Piece {colorArray :: [[Color]]} deriving (Show)
+width :: Piece -> Int
+width (Piece colorArray) = length $ head colorArray
+
+height :: Piece -> Int
+height (Piece colorArray) = length colorArray
+
+prevPoint :: Piece -> BoardPoint -> BoardPoint
+prevPoint piece (BoardPoint 0 y) = BoardPoint (width piece) (y-1)
+prevPoint piece boardPoint = BoardPoint (x boardPoint - 1) (y boardPoint)
+
+maxPoint :: Piece -> BoardPoint
+maxPoint piece = BoardPoint ((width piece) - 1) ((height piece) - 1)
+
+pieceAccess :: Piece -> BoardPoint -> Color
+pieceAccess piece (BoardPoint x y)
+	| not $ isInPiece piece (BoardPoint x y) = error "index out of range"
+	| otherwise = (colorArray piece) !! x !! y
+
+data Player = Player {pieces :: [Piece], color :: Color} deriving (Show)
+
+defaultSize = 20
+newBoard = (Board (take (defaultSize * defaultSize) $ repeat Empty) defaultSize)
+newPlayer = (Player 
+			(
+			 [
+			 Piece [[Red]], 
+			 Piece [[Red, Empty], [Red, Red]],
+			 Piece [[Red, Red]],
+			 Piece [[Red, Red], [Red, Red]],
+			 Piece [[Red, Red, Red]]
+			 ]
+			)
+			 Red)
+
+safeAccess :: [a] -> Int -> [a]
+safeAccess list index = if index < 0 || index >= length list
+					then []
+					else [list !! index]
+
+safeBoardAccess :: Board -> BoardPoint -> [Color]
+safeBoardAccess board point
+	| not $ isInBoard board point = []
+	| otherwise = [boardAccess board point]
+
+corners :: Board -> BoardPoint -> [Color]
+corners board point
+	| not $ isInBoard board point = error "index out of range"
+	| otherwise = (safeBoardAccess board (BoardPoint ((x point)-1) ((y point)-1))) ++ (safeBoardAccess board (BoardPoint ((x point)-1) ((y point)+1))) ++ (safeBoardAccess board (BoardPoint ((x point)+1) ((y point)-1))) ++ (safeBoardAccess board (BoardPoint ((x point)+1) ((y point)+1)))
+
+sides :: Board -> BoardPoint -> [Color]
+sides board point
+	| not $ isInBoard board point = error "index out of range"
+	| otherwise = (safeBoardAccess board $ leftPoint point) ++ (safeBoardAccess board $ rightPoint point) ++ (safeBoardAccess board $ upPoint point) ++ (safeBoardAccess board $ downPoint point)
+
+isOpenToColor :: Board -> Color -> BoardPoint -> Bool
+isOpenToColor board color (BoardPoint 0 0) = boardAccess board (BoardPoint 0 0) == Empty
+isOpenToColor board color point = (color `elem` (corners board point)) && (not (color `elem` sides board point)) && ((boardAccess board point) == Empty)
+
+addPieceSquareToBoard :: Board -> Piece -> BoardPoint -> BoardPoint -> Board
+addPieceSquareToBoard board piece boardLocation (BoardPoint 0 0) = traceShow "adding from piece 0 0 " $ addSquareToBoard board (pieceAccess piece (BoardPoint 0 0)) boardLocation
+addPieceSquareToBoard board piece boardLocation pieceLocation = traceShow (pieceLocation) $ addPieceSquareToBoard (addSquareToBoard 
+																					   board 
+																					   (pieceAccess piece pieceLocation) 
+																					   (boardLocation `plus` pieceLocation)) piece boardLocation (prevPoint piece pieceLocation)
+
+addPieceToBoard :: Board -> Piece -> BoardPoint -> Int -> Board
+addPieceToBoard board piece boardPoint 0 = traceShow boardPoint $ addPieceSquareToBoard board piece boardPoint (maxPoint piece)
+addPieceToBoard _ _ _ rotation = error "not implemented yet"
+
+main = do
+	x <- getLine
+	y <- getLine
+	piece <- getLine
+	putStr $ showToUser $ addPieceToBoard newBoard ((pieces newPlayer) !! read piece) (BoardPoint (read x) (read y)) 0
--- board.hs
+++ board.hs
@@ -1,3 +1,21 @@
+module Board
+(
+	Board(Board),
+	boardAccess,
+	boardIndex,
+	isInBoard,
+	addSquareToBoard,
+	Color(Red, Yellow, Green, Blue, Empty),
+	BoardPoint(BoardPoint, x, y),
+	leftPoint,
+	rightPoint,
+	upPoint,
+	downPoint,
+
+	initOrEmpty,
+	showToUser
+) where
+	
 import Data.List.Split
 
 class ShowToUser a where
@@ -6,10 +24,40 @@ class ShowToUser a where
 instance (ShowToUser a) => ShowToUser [a] where
 	showToUser list = concatMap showToUser list ++ "\n"
 
-data BoardPoint = BoardPoint {x :: Int, y :: Int} deriving (Show)
-isInBoard :: Board -> BoardPoint -> Bool
-isInBoard board point = (x point) >= 0 || (x point) < size board || (y point) >= 0 || (y point) < size board
+initOrEmpty :: [a] -> [a]
+initOrEmpty [] = []
+initOrEmpty list = init list
 
+data Color = Yellow | Red | Green | Blue | Empty deriving (Show, Eq)
+
+instance ShowToUser Color where
+	showToUser Yellow = "Y"
+	showToUser Red = "R"
+	showToUser Green = "G"
+	showToUser Blue = "B"
+	showToUser Empty = "."
+data Board = Board {array :: [Color], size :: Int} deriving (Show)
+
+isInBoard :: Board -> BoardPoint -> Bool
+isInBoard board (BoardPoint x y) = x >= 0 && x < size board && y >= 0 && y < size board
+
+boardAccess :: Board -> BoardPoint -> Color
+boardAccess board point
+	| not $ isInBoard board point = error "index out of range"
+	| otherwise = array board !! boardIndex board point
+
+boardIndex :: Board -> BoardPoint -> Int
+boardIndex board point = ((x point) * (size board)) + (y point)
+
+addSquareToBoard :: Board -> Color -> BoardPoint -> Board
+addSquareToBoard board color point
+	| not $ isInBoard board point = error "index out of range"
+	| otherwise = Board ((initOrEmpty $ fst $ splitAt (boardIndex board point) (array board)) ++ [color] ++ (snd $ splitAt (boardIndex board point) (array board))) (size board)
+
+instance ShowToUser Board where
+	showToUser board = showToUser $ splitEvery 20 (array board)
+
+data BoardPoint = BoardPoint {x :: Int, y :: Int} deriving (Show)
 leftPoint :: BoardPoint -> BoardPoint
 leftPoint (BoardPoint x y) = BoardPoint (x - 1) y
 
@@ -21,104 +69,3 @@ upPoint (BoardPoint x y) = BoardPoint x (y - 1)
 
 downPoint :: BoardPoint -> BoardPoint
 downPoint (BoardPoint x y) = BoardPoint x (y + 1)
-
-data Color = Yellow | Red | Green | Blue | Empty deriving (Show, Eq)
-
-instance ShowToUser Color where
-	showToUser Yellow = "Y"
-	showToUser Red = "R"
-	showToUser Green = "G"
-	showToUser Blue = "B"
-	showToUser Empty = "."
-
-data Board = Board {array :: [Color], size :: Int} deriving (Show)
-
-instance ShowToUser Board where
-	showToUser board = showToUser $ splitEvery 20 (array board)
-
-data Piece = Piece {colorArray :: [[Color]]} deriving (Show)
-width :: Piece -> Int
-width piece = length $ colorArray piece !! 0
-
-prevPoint :: Piece -> BoardPoint -> BoardPoint
-prevPoint piece (BoardPoint 0 y) = BoardPoint (width piece) (y-1)
-prevPoint piece boardPoint = BoardPoint (x boardPoint - 1) (y boardPoint)
-
-maxPoint :: Piece -> BoardPoint
-maxPoint piece = BoardPoint ((width piece) - 1) (length ((colorArray piece) !! 0))
-
-data Player = Player {pieces :: [Piece], color :: Color} deriving (Show)
-
-defaultSize = 20
-newBoard = (Board (take (defaultSize * defaultSize) $ repeat Empty) defaultSize)
-newPlayer = (Player 
-			(
-			 [
-			 Piece [[Red]], 
-			 Piece [[Red, Empty], [Red, Red]],
-			 Piece [[Red, Red]],
-			 Piece [[Red, Red], [Red, Red]],
-			 Piece [[Red, Red, Red]]
-			 ]
-			)
-			 Red)
-
-safeAccess :: [a] -> Int -> [a]
-safeAccess l i = if ((length l) <= i || i < 0)
-					then []
-					else [l !! i]
-
-boardIndex :: Board -> BoardPoint -> Int
-boardIndex board point = ((x point) * (size board)) + (y point)
-
-boardAccess :: Board -> BoardPoint -> Color
-boardAccess board point
-	| not $ isInBoard board point = error "index out of range"
-	| otherwise = array board !! (((x point)*(size board)) + (y point))
-
-safeBoardAccess :: Board -> BoardPoint -> [Color]
-safeBoardAccess board point
-	| not $ isInBoard board point = []
-	| otherwise = [boardAccess board point]
-
-corners :: Board -> BoardPoint -> [Color]
-corners board point
-	| not $ isInBoard board point = error "index out of range"
-	| otherwise = (safeBoardAccess board (BoardPoint ((x point)-1) ((y point)-1))) ++ (safeBoardAccess board (BoardPoint ((x point)-1) ((y point)+1))) ++ (safeBoardAccess board (BoardPoint ((x point)+1) ((y point)-1))) ++ (safeBoardAccess board (BoardPoint ((x point)+1) ((y point)+1)))
-
-sides :: Board -> BoardPoint -> [Color]
-sides board point
-	| not $ isInBoard board point = error "index out of range"
-	| otherwise = (safeBoardAccess board $ leftPoint point) ++ (safeBoardAccess board $ rightPoint point) ++ (safeBoardAccess board $ upPoint point) ++ (safeBoardAccess board $downPoint point)
-
-isOpenToColor :: Board -> Color -> BoardPoint -> Bool
-isOpenToColor board color (BoardPoint 0 0) = (array board) !! 0 == Empty
-isOpenToColor board color point = (color `elem` (corners board point)) && (not (color `elem` sides board point)) && ((boardAccess board point) == Empty)
-
-initOrEmpty :: [a] -> [a]
-initOrEmpty [] = []
-initOrEmpty list = init list
-
-addSquareToBoard :: Board -> Color -> BoardPoint -> Board
-addSquareToBoard board color point
-	| not $ isInBoard board point = error "index out of range"
-	| otherwise = Board ((initOrEmpty $ fst $ splitAt (boardIndex board point) (array board)) ++ [color] ++ (snd $ splitAt (boardIndex board point) (array board))) (size board)
-
-addPieceSquareToBoard :: Board -> Piece -> BoardPoint -> BoardPoint -> Board
-addPieceSquareToBoard board piece boardLocation (BoardPoint 0 0) = addSquareToBoard board 
-																					((colorArray piece) !! (x boardLocation) !! (y boardLocation))
-																					boardLocation
-addPieceSquareToBoard board piece boardLocation pieceLocation = addPieceSquareToBoard (addSquareToBoard 
-																					   board 
-																					   ((colorArray piece) !! (x boardLocation) !! (y boardLocation)) 
-																					   (BoardPoint ((x boardLocation) + (x pieceLocation)) ((y boardLocation) + (y pieceLocation)))) piece boardLocation (prevPoint piece pieceLocation)
-
-addPieceToBoard :: Board -> Piece -> BoardPoint -> Int -> Board
-addPieceToBoard board piece boardPoint 0 = (addPieceSquareToBoard board piece boardPoint) (maxPoint piece)
-addPieceToBoard _ _ _ rotation = error "not implemented yet"
-
-main = do
-	x <- getLine
-	y <- getLine
-	piece <- getLine
-	putStr $ showToUser $ addPieceToBoard newBoard ((pieces newPlayer) !! read piece) (BoardPoint (read x) (read y)) 0
