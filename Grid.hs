99865ba8b091e0d0ed05e334af1983f2ea8e9651
Drew Gross <drew.a.gross@gmail.com>
2012-08-14 00:57:00 -0400

    more refactoring

--- Grid.hs
+++ Grid.hs
@@ -4,7 +4,8 @@ module Grid (
 	height,
 	itemAt,
 	itemIndex,
-	changeItemAt
+	changeItemAt,
+	changeGridAt
 ) where
 	
 import Data.List.Split
@@ -15,16 +16,22 @@ import Utilities
 data Grid t = Grid {array :: [t], width :: Int} deriving (Show)
 
 instance ShowToUser t => ShowToUser (Grid t) where
-	showToUser (Grid array width) = showToUser $ splitEvery width array
+	showToUser = showToUser . rows
 
 height :: Grid t -> Int
 height (Grid array width) = fst $ stupidDivision (length array) width 0 False
+
+rows :: Grid t -> [[t]]
+rows (Grid array width) = splitEvery width array
 
 containsPoint :: Grid t -> Point -> Bool
 containsPoint grid (Point x y) = x >= 0 && x < width grid && y >= 0 && y < height grid
 
 itemIndex :: Grid t -> Point -> Int
-itemIndex grid point = ((y point) * (width grid)) + (x point)
+itemIndex grid (Point x y) = (y * (width grid)) + x
+
+itemPoint :: Grid t -> Int -> Point
+itemPoint (Grid array size) index = Point {x = length array `mod` size, y = length array `div` size}
 
 itemAt :: Grid t -> Point -> t
 itemAt grid point
@@ -38,3 +45,19 @@ changeItemAt grid newItem point
 	| otherwise = 
 		let splitBoard = splitAt (itemIndex grid point) (array grid)
 		in Grid ((initOrEmpty $ fst splitBoard) ++ [newItem] ++ snd splitBoard) (width grid)
+
+changeItemsAt :: Grid t -> [t] -> [Point] -> Grid t
+changeItemsAt grid indexes points
+	| length points == 1 = changeItemAt grid (head indexes) (head points)
+	| otherwise = 
+		let gridWithFirstItemChanged = changeItemAt grid (head indexes) (head points)
+		in changeItemsAt gridWithFirstItemChanged (tail indexes) (tail points)
+
+changeGridAt :: Grid t -> Grid t -> Point -> Grid t
+changeGridAt oldGrid newGrid point
+	| (length $ array oldGrid) == 1 = changeItemAt oldGrid (head $ array oldGrid) point
+	| otherwise = 
+		let 
+		itemList = (array oldGrid)
+		pointList = ([(itemPoint newGrid index) | index <- take (length $ array newGrid) [0..]])
+		in changeItemsAt oldGrid itemList pointList
