405f8ae581ccc3a534437bdbfe9bdee8d695ae8f
Drew Gross <drew.a.gross@gmail.com>
2012-11-07 23:17:23 -0800

    playing no longer relies on amount of players

--- blokus.hs
+++ blokus.hs
@@ -3,9 +3,8 @@ import Player
 import Color
 
 playGame :: (Board, [Player]) -> IO Board
-playGame (board, nextHuman:nextAI:otherPlayers) = do
-	(nextBoard, nextPlayer) <- doTurn nextHuman (board, nextHuman)
-	(nextBoard2, nextPlayer2) <- doTurn nextAI  (nextBoard, nextAI)
-	playGame (nextBoard2, (otherPlayers) ++ [nextPlayer] ++ [nextPlayer2])
+playGame (board, currentPlayer:otherPlayers) = do
+	(nextBoard, finishedPlayer) <- doTurn currentPlayer (board, currentPlayer)
+	playGame (nextBoard, otherPlayers ++ [finishedPlayer])
 
 main = playGame (empty2PlayerBoard, [newHuman Red, newComputer Blue])
