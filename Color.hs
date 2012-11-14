ee4637e674442af741adbfa6b650995a10b93742
Drew Gross <drew.a.gross@gmail.com>
2012-11-14 01:37:28 -0800

    colorized boards

--- Color.hs
+++ Color.hs
@@ -7,9 +7,15 @@ import Display
 
 data Color = Yellow | Red | Green | Blue | Empty deriving (Show, Eq)
 
+colorBlack  = "\ESC[0;30m"
+colorRed    = "\ESC[0;31m"
+colorGreen  = "\ESC[0;32m"
+colorYellow = "\ESC[0;33m"
+colorBlue =   "\ESC[0;34m"
+
 instance Display Color where
-	display Yellow = "Y"
-	display Red = "R"
-	display Green = "G"
-	display Blue = "B"
-	display Empty = "."
+	display Red    = colorRed ++ "R" ++ colorBlack
+	display Green  = colorGreen ++ "G" ++ colorBlack
+	display Yellow = colorYellow ++ "Y" ++ colorBlack
+	display Blue   = colorBlue ++ "B" ++ colorBlack
+	display Empty  = "."
