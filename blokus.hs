2f96712749b34150f48493bc44f7e36f19836d44
Drew Gross <drew.a.gross@gmail.com>
2012-11-17 21:00:54 -0800

    make some chromosomes

--- blokus.hs
+++ blokus.hs
@@ -6,7 +6,10 @@ import Color
 import Display
 import Move
 
-chromosome = [(1.0, squaresUsed),(1.0, launchPointsGained),(1.0, enemyLaunchPointsLost)]
+neutralChromosome = [(1.0, squaresUsed),(1.0, launchPointsGained),(1.0, enemyLaunchPointsLost)]
+agressiveChromosome = [(1.0, squaresUsed),(1.0, launchPointsGained),(2.0, enemyLaunchPointsLost)]
+defensiveChromosome = [(1.0, squaresUsed),(2.0, launchPointsGained),(1.0, enemyLaunchPointsLost)]
+kamikaziChromosome = [(0.5, squaresUsed),(0.0, launchPointsGained),(3.0, enemyLaunchPointsLost)]
 
 playGame :: (Board, [Player]) -> IO ()
 playGame (board, players@(player:enemy:otherPlayers)) = do
@@ -18,4 +21,4 @@ playGame (board, players@(player:enemy:otherPlayers)) = do
 		putStr $ display $ grid nextBoard
 		playGame (nextBoard, enemy:otherPlayers ++ [finishedPlayer])
 
-main = playGame (empty2PlayerBoard, [newComputer Red chromosome, newComputer Blue chromosome])
+main = playGame (empty2PlayerBoard, [newComputer Red agressiveChromosome, newComputer Blue defensiveChromosome])
